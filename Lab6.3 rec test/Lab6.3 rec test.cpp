8d536537df2865358bb2d825f3e6ba8dc3a58433
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-24 01:23:39 +0200

    flx

---
+++ Lab6.3 rec test/Lab6.3 rec test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.3 rec/Lab6.3 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab63rectest
+{
+	TEST_CLASS(Lab63rectest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = 0;
+			int a[5] = { 1, -5, 4, -7, 9 };
+			int i = 0;
+			int max = a[0];
+			t = find_maximum(a, 5, i, max);
+			Assert::AreEqual(t, 9);
+		}
+	};
+}
---
+++ Lab6.3 rec/Lab6.3 rec.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <ctime>
+
+using namespace std;
+
+
+void create(int* a, const int n, const int i, const int HIGH, const int LOW)
+{
+	a[i] = LOW + rand() % (HIGH - LOW + 1);
+	if (i < n - 1)
+		create(a, n, i + 1, HIGH, LOW);
+}
+void print(int* a, const int n, const int i)
+{
+	cout << a[i] << " ";
+	if (i < n - 1)
+	print(a, n, i + 1);
+}
+int find_maximum(int* a, const int n, const int i, int& max)
+
+{
+	if (a[i] >= max)
+		max = a[i];
+	if (i < n - 1)
+		find_maximum(a, n, i + 1, max);
+	else
+		return max;
+}
+
+int main()
+{
+	srand(time(NULL));
+
+
+	const int n = 10;
+	int a[n];
+	const int HIGH = 53;
+	const int LOW = -17;
+	const int i = 0;
+	cout << "a[10] = { ";
+	create(a, n, i, HIGH, LOW);
+	print(a, n, i);
+	cout << "}" << endl;
+	int max = a[0];
+	cout << "F = " << find_maximum(a, n, i, max) << endl;
+}
